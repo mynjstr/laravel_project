dc9e791c5424e05b60f8b167bc17162c6ca36505
jester <jestertiedra@gmail.com>
2026-07-02 15:45:36 +0800

    feat: add automatic activity logging via LogsActivity trait

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Todo extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $fillable = ['title', 'description', 'is_completed'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['title', 'description', 'is_completed'])
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs();
+    }
 }
